dd20edf007266c17bf9ef2fdebc133787a7e4236
pxai <p@pello.io>
2017-08-30 22:13:13 +0200

    Needs fixing

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,11 +4,16 @@ class LoginController < ApplicationController
     end
  
     def sign_in
+      # params[:password]
       @user = User.new(user_params)
+
       
        if !User.find_by(login:@user.login)
+         flash[:notice] = "User not found"
          render 'index'
        else
+         @user = User.find_by(login:@user.login)
+         session[:current_user_id] = @user.id
          redirect_to '/protected/index'
        end
     end
